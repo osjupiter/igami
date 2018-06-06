0de1b3b397c4a82e1de48219bd2f2ddf3a82c8b7
osjupiter <water_totuka@yahoo.co.jp>
2018-06-06 18:36:59 +0900

    asdf

--- src/main/kotlin/com/teikokuvol/bigami/main.kt
+++ src/main/kotlin/com/teikokuvol/bigami/main.kt
@@ -1,10 +1,42 @@
 package com.teikokuvol.bigami
 
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Hello, World!")
-        print("test")
+        println("start up")
+        val vertx = Vertx.vertx()
+        vertx.deployVerticle(Preseitation())
     }
+}
+
+class Preseitation : AbstractVerticle() {
+    var hp = 10
+    override fun start() {
+        var server = vertx.createHttpServer()
+
+        var router = Router.router(vertx)
+
+
+
+        router.route("/info").handler({ request ->
+            // This handler gets called for each request that arrives on the server
+            var response = request.response()
+            response.putHeader("content-type", "text/plain")
+            // Write to the response and end it
+            response.end("Hello World! hp:$hp")
+            damage()
+        })
+        server.requestHandler { router.accept(it) }
+                .listen(8095)
+    }
+
+    fun damage() {
+        hp--
+    }
+
 }
